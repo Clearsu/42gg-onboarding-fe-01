05579e4a530701af847ceff1e0225bccb24898b2
Clearsu <clearwater1219@gmail.com>
2023-10-15 17:02:55 +0900

    [Feat] checkbox #1

--- todo.js
+++ todo.js
@@ -5,10 +5,28 @@ function addTodo (event) {
 
 	if (inputValue.trim()) {
 		const newTodoItem = document.createElement('li');
-		newTodoItem.textContent = inputValue;
+		const checkbox = document.createElement('input');
+		checkbox.type = 'checkbox';
+		checkbox.className = 'todo-checkbox';
+		newTodoItem.appendChild(checkbox);
+		newTodoItem.appendChild(document.createTextNode(inputValue));
 		document.getElementById('todo-list').appendChild(newTodoItem);
 		document.getElementById('todo-input').value = '';
 	}
 }
 
+function checkTodo (event) {
+	if (event.target.className === 'todo-checkbox') {
+        const listItem = event.target.parentElement;
+        if (event.target.checked) {
+            listItem.style.textDecoration = 'line-through';
+			listItem.style.color = 'grey';
+        } else {
+            listItem.style.textDecoration = 'none';
+			listItem.style.color = 'black';
+        }
+    }
+}
+
 document.getElementById('todo-form').addEventListener('submit', addTodo);
+document.getElementById('todo-list').addEventListener('change', checkTodo);
